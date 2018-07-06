b35be9cadd3f9ac6f8b12e70ddb920a5bc858d68
hjones <hjones@192.168.180.70>
2018-07-06 16:54:09 -0400

    Final POC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -19,41 +21,27 @@ export class AppComponent {
   notice = '';
   ed_note = '';
   
+  constructor(private http: HttpClient) { }
+  
   ngOnInit() { 
-      console.log('ngOnItit says Hi');
+      this.http.get('assets/agenda2018b.json').subscribe(data => {
+        console.log(data);
+        this.agenda = <any[]>data;
+        
+        var i : number = 0;
+        var today = new Date().toISOString().slice(0, 10);
+        console.log(today);
       
-      this.agenda = [
-  {  
-      "date":"2018-07-06",
-      "name":"Okkaido Sushi",
-      "address":"245 Eglinton Ave. East",
-      "directions":"at Mt. Pleasant Rd.: take 34, 51, 54, or 56 bus E from Eglinton Station",
-      "phone":"416-483-6848",
-      "notice":"NOTICE !!!",
-      "ed_note":""
-   },
-   {  
-      "date":"2018-07-13",
-      "name":"Okonomi House Restaurant",
-      "address":"23 Charles St. West",
-      "directions":"just W of Yonge, 1 stoplight S of Bloor: walk S from Bloor/Yonge Station",
-      "phone":"416-925-6176",
-      "notice":"",
-      "ed_note":"note by editor"
-   }      ];
-      
-      var i : number = 0;
-      var today = new Date().toISOString().slice(0, 10);
-      console.log(today);
-    
-      for (; i<this.agenda.length; i++) {
-        if (today <= this.agenda[i].date) {
-          this.index = i;
-          break;
+        for (; i<this.agenda.length; i++) {
+          if (today <= this.agenda[i].date) {
+            this.index = i;
+            break;
+          }
         }
-      }
-    
-      this.update(this.index);
+      
+        this.update(this.index);
+      });
+        
   }
   
   onPrevious() {
@@ -67,6 +55,12 @@ export class AppComponent {
   onNavigate(nextPrevious: boolean) {
       console.log('navigate ' + nextPrevious);
       this.index = this.index + (nextPrevious ? 1 : -1);
+      if (this.index < 0) {
+        this.index = 0;
+      }
+      else if (this.index >= this.agenda.length) {
+        this.index = this.agenda.length - 1;
+      }
       this.update(this.index);
   }
   
